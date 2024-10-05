b852a5b3d70dd0d028d990c8a1ad819d45646527
Myuku <junelee2002jh@gmail.com>
2024-10-05 14:20:16 -0700

    Revert "test push"
    This reverts commit 6635e0476059ae366524b98ee5379c1c82cd458d.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,9 @@
 import Image from "next/image";
-import Footer from "./footer"; // Import the Footer component
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
-      <h1 className="text-4xl font-bold text-white">Next.js &times; Defangssss</h1>
-      {/* Other content of the page */}
-      
-      {/* Implement Footer */}
-      <Footer />
+      <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
     </main>
   );
 }
