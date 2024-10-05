ec53230ab3f93124b17e006f30cb4e9d0b8ebc84
Myuku <junelee2002jh@gmail.com>
2024-10-05 16:00:10 -0700

    Added logo

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
-import Message from "./test";
 import Footer from './footer';
+
+import React from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen min-w-screen" style={{ backgroundColor: "#FCFBF6" }}>
       <main className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-black">Next.js &times; Defang</h1>
+        <Image src="/logo.jpeg" alt="Wildlife Watch Logo" width={0} height={0} sizes="100vw" style={{ width: '40%', height: 'auto' }}/>
+
+        {/*   Button to Upload Image  */}
+        
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            UPLOAD IMAGE
+          </span>
+        </button>
+
+
+
       </main>
       <Footer />
     </div>
