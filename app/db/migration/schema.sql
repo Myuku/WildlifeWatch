4a47b7a232a466f4301bdced21bd2d2c81397166
suke404 <mc3chan2003@gmail.com>
2024-10-05 18:51:44 -0700

    changed db

--- app/db/migration/schema.sql
+++ app/db/migration/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE "Animal" (
-  "id" bigserial PRIMARY KEY,
-  "image" bytea NOT NULL,
-  "location" varchar,
-  "animal_name" varchar,
-  "response" varchar,
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    image_url TEXT NOT NULL,
+    location TEXT,
+    animal_type TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
